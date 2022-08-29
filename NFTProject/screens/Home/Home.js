0f2ecc35d74c6d277663fdceb6b9d86391b26020
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-08-29 15:49:55 +0530

    Updated

--- NFTProject/screens/Home/Home.js
+++ NFTProject/screens/Home/Home.js
@@ -3,10 +3,19 @@ import { useState } from "react"
 import { View, SafeArea, FlatList } from "react-native"
 import {COLORS,NFTData} from '../../constants';
 import { SafeAreaView } from "react-native-safe-area-context";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../../Components";
 const Home = () => {
   return (
     <SafeAreaView style={{flex:1}}>
-
+      <FocusedStatusBar background={COLORS.primary}/>
+      <View style={{flex:1}}>
+        <View style={{zIndex:0}} >
+          <FlatList 
+          data={NFTData}
+          renderItem={({item})=><Text>{item.name}</Text>}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
