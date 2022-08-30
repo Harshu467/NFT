952e0286386bb0cb8dc8cf51b8097cc81b5fbaab
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-08-30 16:00:02 +0530

    Updated

--- NFTProject/Components/FocusedStatusBar.js
+++ NFTProject/Components/FocusedStatusBar.js
@@ -4,5 +4,4 @@ const FocusedStatusBar = (props) => {
     const isFocused = useIsFocused();
   return isFocused ? <StatusBar animated={true} {...props} /> : null;
 }
-
 export default FocusedStatusBar
