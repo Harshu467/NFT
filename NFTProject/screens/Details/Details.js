23bb5bd7770b8ddb5d451a69c47737c208ea1965
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-08-30 17:55:27 +0530

    Updated

--- NFTProject/screens/Details/Details.js
+++ NFTProject/screens/Details/Details.js
@@ -1,7 +1,11 @@
-import { Text } from "react-native"
+import { View,Text,Image,StatusBar,FlatList,SafeAreaView } from "react-native"
+import { COLORS, assets,SHADOWS,SIZES,FONTS } from "../../constants"
+
+import { SubInfo, RectButton,CircleButton,FocusedStatusBar,DetailsDesc,DetailsBid } from '../../Components'
 
 const Details = () => {
   return (
+    <View></View>
   )
 }
 
