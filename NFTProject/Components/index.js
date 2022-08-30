d1149dadcf6ee207d37b9f16d255c789374aee36
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-08-30 17:59:02 +0530

    Updated

--- NFTProject/Components/index.js
+++ NFTProject/Components/index.js
@@ -2,6 +2,8 @@ import FocusedStatusBar from "./FocusedStatusBar";
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
 import { CircleButton,RectButton } from "./Button";
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
 export{
-    FocusedStatusBar,HomeHeader,NFTCard,CircleButton,RectButton
+    FocusedStatusBar,HomeHeader,NFTCard,CircleButton,RectButton,DetailsBid,DetailsDesc
 }
