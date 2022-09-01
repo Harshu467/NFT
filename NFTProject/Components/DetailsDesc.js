807aeb32dfe76f7f592e743d034f33c172947510
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-09-01 21:29:32 +0530

    Updated

--- NFTProject/Components/DetailsDesc.js
+++ NFTProject/Components/DetailsDesc.js
@@ -1,9 +1,42 @@
 import { View, Text } from 'react-native'
-const DetailsDesc = () => {
+import { COLORS, FONTS, SIZES } from '../constants'
+import { useState } from 'react'
+import { EthPrice, NFTTitle } from './SubInfo'
+const DetailsDesc = ({ data }) => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <>
+      <View style={{
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.extraLarge}
+          subTitleSize={SIZES.font}
+        />
+        <EthPrice
+          price={data.price}
+        />
+      </View>
+      <View style={{ marginVertical: SIZES.extraLarge * 1.5 }}>
+        <Text style={{
+          fontSize: SIZES.small,
+          fontFamily: FONTS.semiBold,
+          color: COLORS.primary,
+        }}>Description</Text>
+        <View style={{ marginVertical: SIZES.base}}>
+          <Text style={{
+            fontSize: SIZES.small,
+            fontFamily: FONTS.regular,
+            color: COLORS.secondary,
+            lineHeight:SIZES.large,
+          }}>{data.description}</Text>
+        </View>
+      </View>
+    </>
   )
 }
 
